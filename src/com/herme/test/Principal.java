9d22a39e499d69056548eae5a7f36602d87bec8b
Herme Ambriz <hambriz@hotmail.com>
2015-04-07 00:04:06 -0500

    agregar node001 a repositorio

---
+++ src/com/herme/test/Principal.java
@@ -0,0 +1,16 @@
+package com.herme.test;
+
+public class Principal {
+	
+	public String saludar(String x){
+		return ("Hola, " + x );
+	}
+
+	public int sumar( int n1, int n2){
+		return n1 + n2;
+	}
+	
+	public String mensaje(){
+		return "Si ves este mensaje entonce tu eres la resistencia";
+	}
+}
